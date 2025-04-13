d520db136c20c8bf92fb51877ec6c9ec68e38e10
Vishnu V <vishnuvirat564@gmail.com>
2025-04-13 12:49:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+let i=1;
+document.getElementById("next").addEventListener("click", ()=>{
+	   i++;
+	    if(i==1){
+			document.getElementById("prev").style.display="none";
+		}
+	   else{
+		  document.getElementById("prev").style.display="block"; 
+	   }
+	    if(i==5){
+			document.getElementById("next").style.display="none";
+		}
+	   else{
+		   document.getElementById("next").style.display="block";
+	   }
+        
+	   
+	    document.getElementById(`circle-${i}`).classList.add("active");
+})
+document.getElementById("prev").addEventListener("click", ()=>{
+	   i--;
+	   if(i==1){
+			document.getElementById("prev").style.display="none";
+		}
+	else{
+		  document.getElementById("prev").style.display="block"; 
+	   }
+	    if(i==5){
+			document.getElementById("next").style.display="none";
+		}
+	else{
+		   document.getElementById("next").style.display="block";
+	   }
+	
+	 document.getElementById(`circle-${i+1}`).classList.remove("active");
+	 
+})
